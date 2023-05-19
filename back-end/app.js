4caae2e59654793af9512f85763c72d2f82d8811
EgleeB <117934410+EgleeB@users.noreply.github.com>
2023-05-19 09:56:19 +0300

    wrote queries for tables

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
---
+++ back-end/db.js
@@ -0,0 +1,56 @@
+const mysql = require("mysql2");
+
+const databaseConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  port: process.env.DB_PORT,
+};
+
+const dbConnection = mysql.createConnection({
+  ...databaseConfig,
+  database: "",
+});
+
+dbConnection.query("CREATE DATABASE IF NOT EXISTS final_project", (err) => {
+  if (err) throw err;
+  console.log("Database created");
+
+  dbConnection.query("USE final_project", (err) => {
+    if (err) throw err;
+    console.log("Using final_project database");
+
+    const adminTableQuery = `CREATE TABLE IF NOT EXISTS admin (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            first_name VARCHAR(255) NOT NULL,
+            last_surname VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL;
+            )`;
+
+    const participantsTableQuery = `
+            CREATE TABLE IF NOT EXISTS participants (
+              id INT PRIMARY KEY AUTO_INCREMENT,
+              first_name VARCHAR(255) NOT NULL,
+              last_surname VARCHAR(255) NOT NULL,
+              email VARCHAR(255) NOT NULL UNIQUE,
+              phone_number VARCHAR(255) NOT NULL,
+              admin_id INT,
+              FOREIGN KEY (admin_id) REFERENCES admin(id)
+            )
+          `;
+
+    dbConnection,
+      query(adminTableQuery, (err) => {
+        if (err) throw err;
+        console.log("admin table created");
+
+        dbConnection.query(participantsTableQuery, (err) => {
+          if (err) throw err;
+          console.log("participants table created");
+        });
+      });
+  });
+});
+
+module.exports = { dbConnection };
